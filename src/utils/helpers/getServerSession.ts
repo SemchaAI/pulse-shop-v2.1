7594c1b6eb6a1cddd63a761648b997252eda5edd
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-01-24 14:09:02 +0200

    feat: server side session control helper

---
+++ src/utils/helpers/getServerSession.ts
@@ -0,0 +1,50 @@
+'use server';
+// import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+import { jwtVerify } from 'jose';
+import { JWT_ACCESS } from '../consts/env';
+
+import type { IUserResponse, IUserSession } from '@/models/auth';
+
+export async function getServerSession(): Promise<IUserResponse> {
+  const cookieStore = await cookies();
+  const refreshToken = cookieStore.get('refreshToken');
+  if (!refreshToken) {
+    return {
+      user: null,
+      message: 'Unauthorized',
+      status: 200,
+    };
+  }
+  const accessToken = cookieStore.get('accessToken');
+  if (!accessToken) {
+    return {
+      user: null,
+      message: 'Unauthorized missing token',
+      status: 401,
+    };
+  }
+  if (!JWT_ACCESS) {
+    return {
+      user: null,
+      message: 'Server configuration error',
+      status: 500,
+    };
+  }
+
+  let userData: IUserSession | null = null;
+  try {
+    // userData = jwt.verify(accessToken.value, JWT_ACCESS) as IUserSession;
+    const accessSecret = new TextEncoder().encode(JWT_ACCESS);
+    const { payload } = await jwtVerify(accessToken.value, accessSecret);
+    console.log('jwtVerify', payload);
+    userData = payload as unknown as IUserSession;
+    return { user: userData, status: 200, message: 'User found' };
+  } catch (err) {
+    return {
+      user: null,
+      message: `[GET SERVER SESSION] ${err}`,
+      status: 401,
+    };
+  }
+}
