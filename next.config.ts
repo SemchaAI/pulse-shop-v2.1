9194b1183027586310a03539ab28f175989bef1e
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-01-24 13:38:31 +0200

    feat(config): add support for remote images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_BASE_ADDRESS!,
+        // port: process.env.NEXT_PUBLIC_BASE_PORT || '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_BASE_ADDRESS3!,
+        pathname: '**',
+      },
+    ],
+  },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
