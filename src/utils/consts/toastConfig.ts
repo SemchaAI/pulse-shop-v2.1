3021d8ee11ea34e909ed7fc126a4c010073226fc
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-01-24 14:10:19 +0200

    feat: toast style config

---
+++ src/utils/consts/toastConfig.ts
@@ -0,0 +1,9 @@
+export const toastOptions = {
+  className: '',
+  style: {
+    border: '1px solid var(--border)',
+    padding: '10px',
+    color: 'var(--text-primary)',
+    background: 'var(--background',
+  },
+};
