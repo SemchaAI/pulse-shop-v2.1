1a2f72da1fd0ec798ea31443932043a471c2523f
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-01-24 14:05:34 +0200

    reexport utils for readable import

---
+++ src/utils/index.ts
@@ -0,0 +1,8 @@
+export {
+  generateTokens,
+  validateAccessToken,
+  validateRefreshToken,
+} from './api/helpers/jwt';
+
+export { sendActivationMail } from './mail';
+export { queryClient } from './queryClient';
