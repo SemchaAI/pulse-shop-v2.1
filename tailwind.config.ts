4d50284ecc6c6101bcaa4762f25bf3b6c03ef3e6
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-01-24 14:00:06 +0200

    feat(tailwind): extend theme with custom colors, fonts, and utilities
    - Added custom colors for background, foreground, action, primary, secondary, feedback, and text.
    - Configured a custom maxWidth (`custom-xl`) for responsive design.
    - Added new font families: `Inter` and `Nunito`.
    - Enabled dark mode using a selector with `[data-mode="dark"]`.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,56 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      darkMode: ['selector', '[data-mode="dark"]'],
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // Background Colors
+        background: 'var(--background)',
+        // Foreground Colors
+        foreground: 'var(--foreground)',
+        //Action Colors
+        'action-disabled': 'var(--action-disabled)',
+        'action-disabled-bg': 'var(--action-disabled-bg)',
+        // Primary Colors
+        primary: 'var(--primary)',
+        'primary-action': 'var(--primary-action)',
+        // Secondary Colors
+        secondary: 'var(--secondary)',
+        'black-01': 'var(--black-01)',
+        'black-02': 'var(--black-02)',
+
+        // Feedback Colors
+        error: 'var(--error)',
+        warning: 'var(--warning)',
+        success: 'var(--success)',
+
+        // Border Colors
+        border: 'var(--border)',
+
+        // Text Colors
+        'text-primary': 'var(--text-primary)',
+        'text-secondary': 'var(--text-secondary)',
+        'text-highlight': 'var(--text-highlight)',
+        'text-error': 'var(--text-error)',
+        'text-warning': 'var(--text-warning)',
+        'text-success': 'var(--text-success)',
+      },
+      maxWidth: {
+        'custom-xl': '1440px', //87.5rem
+      },
+      padding: {
+        // 'custom-lg': '5rem', // Example custom padding
+        // 'custom-sm': '2rem',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'], // Default fallback
+        nunito: ['Nunito', 'sans-serif'], // Add custom fonts
       },
     },
   },
