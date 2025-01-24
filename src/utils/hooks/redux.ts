1fa1a369a75463dfb3b644c4c670e12be3fe5f4a
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-01-24 14:04:00 +0200

    feat: custom redux dispatcher and selector with types

---
+++ src/utils/hooks/redux.ts
@@ -0,0 +1,9 @@
+import { AppDispatch, RootState } from '@/redux/store';
+import {
+  useSelector,
+  type TypedUseSelectorHook,
+  useDispatch,
+} from 'react-redux';
+
+export const useAppDispatch = useDispatch<AppDispatch>;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
