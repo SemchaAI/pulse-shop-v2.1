b35676b4af265ce615c05273b5e798f39a958b18
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-01-24 14:13:51 +0200

    feat: reexport constants for readable imports

---
+++ src/utils/consts/index.ts
@@ -0,0 +1,18 @@
+export {
+  PhoneValidation,
+  Min5,
+  ReqMin5Max20Validation,
+  emailValidation,
+  required,
+} from './validationObjects';
+export type { ValidationTypes } from './validationObjects';
+export {
+  JWT_ACCESS,
+  JWT_REFRESH,
+  NEXT_PUBLIC_SERVER_URL,
+  GOOGLE_ID,
+  GOOGLE_SECRET,
+} from './env';
+export { queryKeys } from './queryKeys';
+export { toastOptions } from './toastConfig';
+export { ROUTES, API_ROUTES } from './routes';
