89eb3858c1d98f5ee4c68550379fdaf7ea660955
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-01-24 14:14:55 +0200

    feat(env): export environment variables for better readability and global state control

---
+++ src/utils/consts/env.ts
@@ -0,0 +1,6 @@
+export const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
+export const JWT_ACCESS = process.env.NEXT_JWT_ACCESS_SECRET;
+export const JWT_REFRESH = process.env.NEXT_JWT_REFRESH_SECRET;
+
+export const GOOGLE_ID = process.env.GOOGLE_ID;
+export const GOOGLE_SECRET = process.env.GOOGLE_SECRET;
