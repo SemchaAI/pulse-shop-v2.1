9c01ccfd01cedcd210b603489fada2ea3b2cce11
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-01-24 14:06:14 +0200

    feat: custom session hook for user state control

---
+++ src/utils/hooks/useSession.ts
@@ -0,0 +1,28 @@
+'use client';
+import { useQuery } from '@tanstack/react-query';
+import type { IUserSession } from '@/models/auth';
+import { getSession } from '../api';
+import { queryKeys } from '@/utils/consts';
+
+type UseSessionReturn = {
+  data: IUserSession | null;
+  status: 'loading' | 'authenticated' | 'unauthenticated';
+};
+export const useSession = (): UseSessionReturn => {
+  const { data, status } = useQuery({
+    queryKey: queryKeys.session,
+    queryFn: getSession,
+    //staleTime: 5 * 60 * 1000, // Cache for 5 minutes
+    refetchOnWindowFocus: true,
+  });
+
+  return {
+    data: data ?? null,
+    status:
+      status === 'pending'
+        ? 'loading'
+        : data
+        ? 'authenticated'
+        : 'unauthenticated',
+  };
+};
