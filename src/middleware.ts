2678b4f892ad462a218b075c508336c024937d54
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-01-25 18:59:04 +0200

    feat(middleware): add authentication and role-based access control

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,38 @@
+import { jwtVerify } from 'jose';
 import { NextRequest, NextResponse } from 'next/server';
-import { getServerSession } from './utils/helpers';
+import { JWT_REFRESH } from './utils/consts';
+import { IUserSession } from './models/auth';
+import { Role } from '@prisma/client';
+
+const JWT_REFRESH_SECRET = new TextEncoder().encode(JWT_REFRESH);
+
 export default async function middleware(req: NextRequest) {
-  console.log('middleware');
-  const { user, message } = await getServerSession();
-  console.log('data', user, message);
-  if (!user) {
+  console.log('MIDDLEWARE init');
+  const refreshToken = req.cookies.get('refreshToken')?.value;
+  if (!refreshToken) {
     return NextResponse.redirect(new URL('/login', req.url));
+  } else if (refreshToken) {
+    try {
+      const { payload } = await jwtVerify(refreshToken, JWT_REFRESH_SECRET);
+      const decoded = payload as unknown as IUserSession;
+      if (!decoded) {
+        return NextResponse.redirect(new URL('/login', req.url));
+      }
+      const tokenExpiration = payload.exp ? new Date(payload.exp * 1000) : null;
+      if (!tokenExpiration || tokenExpiration <= new Date()) {
+        return NextResponse.redirect(new URL('/login', req.url));
+      }
+      const isAdminPath = req.nextUrl.pathname.includes('/admin');
+      console.log('isAdminPath', isAdminPath);
+      if (isAdminPath && decoded.role !== Role.ADMIN) {
+        return NextResponse.redirect(new URL('/', req.url));
+      }
+    } catch {
+      return NextResponse.redirect(new URL('/login', req.url));
+    }
   }
+  return NextResponse.next();
 }
 export const config = {
-  matcher: ['/profile', '/protected/:path*'],
+  matcher: ['/profile', '/protected/:path*', '/admin/:path*'],
 };
