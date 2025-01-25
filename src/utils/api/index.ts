ba290e85e4120f8bee40f244d0d69106a175993f
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-01-25 19:05:40 +0200

    feat: reexport for readable import

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -5,5 +5,6 @@ export {
 } from './helpers/jwt';
 
 export { customFetch } from './helpers/customFetch';
+export { authMiddleware } from './helpers/authMiddleware';
 
 export { getSession, credentialsSignup } from './http';
