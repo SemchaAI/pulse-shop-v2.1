9048f7a96f45b73ef3b4f494ac8c4e11eeba8ce6
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-01-25 19:36:16 +0200

    feat: reexport entities for improve import readability

---
+++ src/components/entities/index.ts
@@ -0,0 +1,4 @@
+export { ProductsCarousel } from './productsCarousel/ProductsCarousel';
+export { ProductsCard } from './productsCard/ProductsCard';
+export { FieldInput } from './fieldInput/FieldInput';
+export { Form } from './form/Form';
