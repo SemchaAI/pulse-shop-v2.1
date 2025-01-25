e121a74928a40df727fec544e0cab2db3ae92177
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-01-25 19:39:02 +0200

    feat: reexport features for improve import readability

---
+++ src/components/features/index.ts
@@ -0,0 +1,8 @@
+export { ThemeSwitcher } from './theme/ThemeSwitcher';
+export {
+  NextButton,
+  PrevButton,
+  usePrevNextButtons,
+} from './carouselControls/ArrowButtons';
+export { RegisterForm } from './forms/RegisterForm';
+export { LoginForm } from './forms/LoginForm';
