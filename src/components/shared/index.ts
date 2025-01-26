c0e3536fc697858b4ac79c444bb5916aaba5e7b1
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-01-26 15:50:02 +0200

    feat: reexport for readable import

---
+++ src/components/shared/index.ts
@@ -0,0 +1,5 @@
+export { Container } from './container/Container';
+export { NavLink } from './navLink/NavLink';
+export { Input } from './input/Input';
+export { Skeleton } from './skeleton/Skeleton';
+export { Button } from './button/Button';
