c87a0184486df395ccb922bc84b9857300fa9f57
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-01-26 15:50:07 +0200

    feat: reexport for readable import

---
+++ src/components/widgets/index.ts
@@ -0,0 +1,4 @@
+export { Footer } from './footers/Footer';
+export { Header } from './headers/Header';
+export { ProductsSection } from './productsSection/ProductsSection';
+export { AuthSection } from './sections/AuthSection';
