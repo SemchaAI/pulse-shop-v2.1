263ffee6c121a315c520447c99720af91efb39dd
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-02-13 14:41:31 +0200

    feat: sending critical data to redux to reduce rerenders and have one point of loading critical data

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,13 +2,22 @@
 import { toastOptions } from '@/utils/consts/toastConfig';
 import { Toaster } from 'react-hot-toast';
 import { Provider as ReduxProvider } from 'react-redux';
-import { appStore } from '@/redux/store';
+import { makeStore } from '@/redux/store';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/utils';
+import { CriticalData } from '@/models/cart';
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({
+  children,
+  criticalData,
+}: {
+  children: React.ReactNode;
+  criticalData: CriticalData;
+}) {
+  const store = makeStore(criticalData);
+  console.log('initInitInit');
   return (
-    <ReduxProvider store={appStore}>
+    <ReduxProvider store={store}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
       <Toaster
         position="top-left"
