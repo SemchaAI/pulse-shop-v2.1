939ffdb984efffee09c89002f97d1754cd1d1003
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-02-13 15:06:27 +0200

    feat(store): update store initialization with preloaded state for cart
    -added cart slice in `combineReducers`
    - Updated `RootState` and `AppDispatch` type inference
    - Modified `makeStore` function to accept `preloadedState` parameter

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,30 +1,39 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { sessionSlice } from './user';
+import { cartSlice } from './cart';
+import { CriticalData } from '@/models/cart';
 
-// import { productsSlice } from '@/redux/products/slice';
-// import { filterSlice } from '@/redux/filter/slice';
-// import { cartSlice } from '@/redux/cart/slice';
-// import { setupListeners } from '@reduxjs/toolkit/query';
 const rootReducer = combineReducers({
   [sessionSlice.name]: sessionSlice.reducer,
-  // [productsSlice.name]: productsSlice.reducer,
-  // [filterSlice.name]: filterSlice.reducer,
-  // [cartSlice.name]: cartSlice.reducer,
-  // [baseApi.reducerPath]: baseApi.reducer,
+  [cartSlice.name]: cartSlice.reducer,
 });
 
-export function makeStore() {
+export function makeStore(preloadedState?: CriticalData) {
+  console.log('preloadedState', preloadedState);
   const store = configureStore({
     devTools: true,
     reducer: rootReducer,
+    preloadedState: preloadedState
+      ? {
+          cart: {
+            cartProducts: [],
+            totalAmount: 0,
+            cartTotal: preloadedState.cartTotal,
+            loading: false,
+            error: null,
+          },
+        }
+      : undefined,
   });
   return store;
 }
 
-export const appStore = makeStore();
-
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof appStore.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof appStore.dispatch;
+export type AppDispatch = ReturnType<typeof makeStore> extends {
+  dispatch: infer D;
+}
+  ? D
+  : never;
 // Export a reusable type for handwritten thunks
