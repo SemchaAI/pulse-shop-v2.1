0e68c6ae691d3fbc2f757816afc0969ff7f6411d
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-02-13 12:12:01 +0200

    fix(and update): api routes to absolute path + new paths
    ROUTES
    -categories path
    API_ROUTES
    -filters path
    -guest path
    -cart path

--- src/utils/consts/routes.ts
+++ src/utils/consts/routes.ts
@@ -8,12 +8,16 @@ export const ROUTES = {
   CART: '/cart',
   FAVORITES: '/favorites',
   PROFILE: '/profile',
+  CATEGORIES: '/categories',
 };
 export const API_ROUTES = {
-  REFRESH: 'api/auth/refresh',
-  ACTIVATION: 'api/auth/activation',
-  SESSION: 'api/auth/session',
-  CREDENTIALS: 'api/auth/credentials',
-  GOOGLE: 'api/auth/google',
+  FILTERS: '/api/filters',
+  REFRESH: '/api/auth/refresh',
+  ACTIVATION: '/api/auth/activation',
+  SESSION: '/api/auth/session',
+  CREDENTIALS: '/api/auth/credentials',
+  GUEST: '/api/auth/credentials/guest',
+  GOOGLE: '/api/auth/google',
   GOOGLE_CALLBACK: `${NEXT_PUBLIC_SERVER_URL}/auth/google/callback`,
+  CART: '/api/cart',
 };
