c04030676618c94d7eaffe14fcf7d79cedfeec18
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-02-13 12:40:46 +0200

    feat: not found global page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import { SearchX } from 'lucide-react';
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col gap-3 justify-center items-center h-full">
+      <h2 className="flex gap-3 text-primary">
+        Not Found <SearchX />
+      </h2>
+      <p>Could not find requested resource</p>
+      <Link
+        className="text-primary"
+        href="/"
+      >
+        Return Home
+      </Link>
+    </div>
+  );
+}
