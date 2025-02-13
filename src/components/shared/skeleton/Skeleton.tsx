bbf65eea513da068a311c5d0531a45457d55768d
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-02-13 12:36:34 +0200

    feat: add support for custom classnames in skeleton for better flexibility

--- src/components/shared/skeleton/Skeleton.tsx
+++ src/components/shared/skeleton/Skeleton.tsx
@@ -2,15 +2,19 @@ interface IProps {
   width?: string;
   height?: string;
   borderRadius?: string;
+  className?: string;
 }
 export const Skeleton = ({
   width = '100%',
   height = 'auto',
   borderRadius = '0',
+  className,
 }: IProps) => {
   return (
     <div
-      className="animate-pulse bg-action-disabled"
+      className={`animate-pulse bg-action-disabled ${
+        className ? className : ''
+      }`}
       style={{ width, height, borderRadius }}
     ></div>
   );
