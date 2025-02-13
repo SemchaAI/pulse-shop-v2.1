48b9d840d78ed86300f607dba48ff58aef51d4f3
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-02-13 12:27:45 +0200

    fix: changed interface name from IProductVariant to ISearchProductVariant
    already have IProductVariant in another model file

--- src/models/prisma.ts
+++ src/models/prisma.ts
@@ -8,6 +8,6 @@ export interface IProduct extends Product {
   variants: ProductVariant[];
 }
 
-export interface IProductVariant extends ProductVariant {
+export interface ISearchProductVariant extends ProductVariant {
   images: Image[];
 }
