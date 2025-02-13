5157d8f055a9e41db856836e670bb865246e81a0
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-02-13 12:00:37 +0200

    refactor: Update `userSessionAdapter` to map user session data
    - Adjusts `userSessionAdapter` to return session data from `User` model
    - Removed unused cart and favorite fields (previously commented out)
    - Added provider name to essential fields
    - Refines user session data mapping to include essential fields (id, name, email, provider, role, avatar)

--- src/utils/helpers/userSessionAdapter.ts
+++ src/utils/helpers/userSessionAdapter.ts
@@ -1,12 +1,27 @@
 import { IUserSession } from '@/models/auth';
 import { User } from '@prisma/client';
 
+// interface IUser extends User {
+//   cart: {
+//     _count: {
+//       cartProducts: number;
+//     };
+//   } | null;
+//   favorite: {
+//     _count: {
+//       favoriteProducts: number;
+//     };
+//   } | null;
+// }
 export const userSessionAdapter = (user: User): IUserSession => {
   return {
     id: user.id,
     name: user.name,
     email: user.email,
+    provider: user.provider,
     role: user.role,
     avatar: user.avatar,
+    // cartTotal: user.cart ? user.cart._count.cartProducts : 0,
+    // favoriteTotal: user.favorite ? user.favorite._count.favoriteProducts : 0,
   };
 };
