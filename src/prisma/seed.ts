812ab67648d0f0b8fcdfa6a49a8b78642bbe0176
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-02-13 11:44:00 +0200

    refactor: new version of seed data with prisma for test project

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,63 +1,39 @@
 import {
-  Categories,
-  Colors,
-  Memory,
-  ProductInfo,
-  ProductItemsSimple,
-  ProductItemsSmartphones,
-  ProductItemThumbnails,
-  Products,
-  RAM,
-} from './consts';
+  CategoriesInit,
+  ColorsInit,
+  MemoryInit,
+  RAMInit,
+  TagsInit,
+} from './consts/simple';
 import { prisma } from './prismaClient';
+import { Poco } from './consts/poco';
+import { Redmi } from './consts/redmi';
+import { Pads } from './consts/pads';
+import { Accessories } from './consts/accessories';
+import { Xiaomi14T } from './consts/tSeria';
+import { Iphone16 } from './consts/iphone';
+
+export interface ISeedProps {
+  categories: Awaited<ReturnType<typeof CategoriesInit>>;
+  colors: Awaited<ReturnType<typeof ColorsInit>>;
+  memory: Awaited<ReturnType<typeof MemoryInit>>;
+  ram: Awaited<ReturnType<typeof RAMInit>>;
+  tags: Awaited<ReturnType<typeof TagsInit>>;
+}
 
 async function up() {
-  await prisma.category.createMany({
-    data: Categories.map(({ id, ...props }) => props),
-  });
-  ///-------------------------------
-  await prisma.color.createMany({
-    data: Colors.map(({ id, ...props }) => props),
-  });
-  await prisma.memory.createMany({
-    data: Memory.map(({ id, ...props }) => props),
-  });
-  await prisma.ram.createMany({
-    data: RAM.map(({ id, ...props }) => props),
-  });
-  ///-------------------------------
+  const categories = await CategoriesInit();
+  const colors = await ColorsInit();
+  const memory = await MemoryInit();
+  const ram = await RAMInit();
+  const tags = await TagsInit();
 
-  const products = Products.map(({ id, ...props }) => props);
-
-  for (let i = 0; i < products.length; i++) {
-    await prisma.product.create({
-      data: {
-        ...products[i],
-        colors: {
-          connect: products[i].colors,
-        },
-        ram: {
-          connect: products[i].ram,
-        },
-        memory: {
-          connect: products[i].memory,
-        },
-      },
-    });
-  }
-
-  ///-------------------------------
-  await prisma.productInfo.createMany({
-    data: ProductInfo,
-  });
-
-  //
-  await prisma.productItem.createMany({
-    data: [...ProductItemsSmartphones, ...ProductItemsSimple],
-  });
-  await prisma.productImages.createMany({
-    data: [...ProductItemThumbnails],
-  });
+  await Poco({ categories, colors, memory, ram, tags });
+  await Redmi({ categories, colors, memory, ram, tags });
+  await Pads({ categories, colors, memory, ram, tags });
+  await Accessories({ categories, colors, memory, ram, tags });
+  await Xiaomi14T({ categories, colors, memory, ram, tags });
+  await Iphone16({ categories, colors, memory, ram, tags });
 }
 
 async function down() {
@@ -66,12 +42,14 @@ async function down() {
   await prisma.$executeRaw`TRUNCATE TABLE "Color" RESTART IDENTITY CASCADE`;
   await prisma.$executeRaw`TRUNCATE TABLE "Memory" RESTART IDENTITY CASCADE`;
   await prisma.$executeRaw`TRUNCATE TABLE "Ram" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Tag" RESTART IDENTITY CASCADE`;
   await prisma.$executeRaw`TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE`;
-  await prisma.$executeRaw`TRUNCATE TABLE "ProductItem" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "ProductVariant" RESTART IDENTITY CASCADE`;
   await prisma.$executeRaw`TRUNCATE TABLE "ProductInfo" RESTART IDENTITY CASCADE`;
-  await prisma.$executeRaw`TRUNCATE TABLE "ProductImages" RESTART IDENTITY CASCADE`;
-  // await prisma.$executeRaw`TRUNCATE TABLE "Cart" RESTART IDENTITY CASCADE`;
-  // await prisma.$executeRaw`TRUNCATE TABLE "CartProduct" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Image" RESTART IDENTITY CASCADE`;
+  // await prisma.$executeRaw`TRUNCATE TABLE "ProductImages" RESTART IDENTITY CASCADE`;
+  // // await prisma.$executeRaw`TRUNCATE TABLE "Cart" RESTART IDENTITY CASCADE`;
+  // // await prisma.$executeRaw`TRUNCATE TABLE "CartProduct" RESTART IDENTITY CASCADE`;
 }
 
 async function main() {
