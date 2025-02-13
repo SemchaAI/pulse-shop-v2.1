682ed329f81b0bd73d7dbdc61533fd3399556bb5
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-02-13 12:39:41 +0200

    fix: set default className to "" and adjust style based on rounded prop

--- src/components/shared/input/Input.tsx
+++ src/components/shared/input/Input.tsx
@@ -2,11 +2,13 @@ import { forwardRef } from 'react';
 import type { IInput } from '@/models/inputs';
 
 export const Input = forwardRef<HTMLInputElement, IInput>(
-  ({ className, ...props }, ref) => {
+  ({ className = '', rounded, ...props }, ref) => {
     return (
       <input
         ref={ref}
-        className={`w-full p-[10px] outline-none border border-border rounded-md text-text-primary bg-background transition-colors focus:border-primary autofill:caret-transparent ${className}`}
+        className={`${
+          rounded ? 'rounded-full py-2 px-4' : 'rounded-md p-2'
+        } w-full  outline-none border border-border text-text-primary bg-background transition-colors focus:border-primary autofill:caret-transparent ${className}`}
         {...props}
       />
     );
