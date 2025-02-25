34b7885248565733ca47f55c54234135a0a8defc
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-02-25 15:35:45 +0200

    feat(cart): integrate React Query for cart fetching
    - from prefetched cart data to client side fetching with useQuery.
    - Implemented loading, error, and empty cart states.
    - Moved cart display and controls into separate components.
    - Sync data with redux using `useEffect`

--- src/components/widgets/sections/CartSection.tsx
+++ src/components/widgets/sections/CartSection.tsx
@@ -1,35 +1,75 @@
 'use client';
-import { Container, EntityBlock } from '@/components/shared';
-import type { ICartSliceData } from '@/models/cart';
-import { initCart } from '@/redux/cart';
-import { useAppDispatch, useAppSelector } from '@/utils/hooks';
 import { useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { Box, Loader2 } from 'lucide-react';
+import { useAppDispatch } from '@/utils/hooks';
 
-interface IProps {
-  cart: ICartSliceData;
-}
+import { CartProduct } from '@/components/entities';
+import { Container, EntityBlock, StateContainer } from '@/components/shared';
+import { initCart } from '@/redux/cart';
+import { getCart } from '@/utils/api/http/cartApi';
+import { queryKeys } from '@/utils/consts';
+import type { ICartSliceData } from '@/models/cart';
 
-export const CartSection = ({ cart: cartData }: IProps) => {
+export const CartSection = () => {
+  console.log('CartSection');
   const dispatch = useAppDispatch();
-  const cart = useAppSelector((state) => state.cart);
+  // const cartSelector = useAppSelector((state) => state.cart);
+
+  const {
+    data: cart,
+    isLoading,
+    error,
+  } = useQuery<ICartSliceData>({
+    queryKey: [queryKeys.cart],
+    queryFn: getCart,
+    // initialData: cartSelector,
+    retry: 1, // custom fetch will check for 401 error other errors only once
+  });
 
   useEffect(() => {
-    if (cartData) {
-      dispatch(initCart(cartData));
-    }
-  }, [dispatch, cartData]);
+    if (cart) dispatch(initCart(cart));
+  }, [dispatch, cart]);
+
+  if (error)
+    return (
+      <StateContainer>
+        Error. Cart wasn`t loaded. {error?.message}
+      </StateContainer>
+    );
+  if (isLoading || !cart)
+    return (
+      <StateContainer>
+        Loading...
+        <Loader2
+          className="animate-spin"
+          size={24}
+        />
+      </StateContainer>
+    );
+  if (cart.cartProducts.length === 0) {
+    return (
+      <StateContainer>
+        <p>Cart is empty</p>
+        <Box size={36} />
+      </StateContainer>
+    );
+  }
   return (
     <section className="my-5">
       <Container>
         <h1 className="typo-title-30">Cart</h1>
-
         <div className="mt-5 flex gap-5">
           <EntityBlock className="flex-grow">
             <ul className="h-[740px] flex flex-col items-center gap-2 flex-grow p-0 overflow-y-auto overflow-x-hidden custom-scrollbar">
               {cart &&
-                cart.cartProducts.map((product) => {
-                  return <li key={product.id}>{product.name}</li>;
-                })}
+                cart.cartProducts.map((product, index) => (
+                  <CartProduct
+                    key={product.id}
+                    index={index}
+                    item={product}
+                  />
+                ))}
             </ul>
           </EntityBlock>
           <EntityBlock className="w-96 h-fit sticky top-5">
@@ -43,10 +83,8 @@ export const CartSection = ({ cart: cartData }: IProps) => {
               <p>Total price:</p>
               <p>{cart.totalAmount} MDL</p>
             </div>
-            {/* CartControls */}
           </EntityBlock>
         </div>
-        <div className="h-80"></div>
       </Container>
     </section>
   );
