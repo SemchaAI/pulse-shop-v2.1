e643061817263ff2484e60b1e7bde19b5d7bf59e
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-02-25 16:23:46 +0200

    feat(ProductsSection): enhance flexibility and loading behavior
    - Added `withPriority` and `skeletons` props to control the number of priority items and skeleton loaders.
    - Improved layout with customizable skeleton loader count and priority logic for product rendering.
    - Enhanced flexibility in controlling the number of products images loading priority.

--- src/components/widgets/productsSection/ProductsSection.tsx
+++ src/components/widgets/productsSection/ProductsSection.tsx
@@ -5,25 +5,31 @@ interface IProps {
   category: string;
   products: ISearchProductVariant[];
   isLoading: boolean;
+  withPriority?: number;
+  skeletons?: number;
 }
 
-export const ProductsSection = ({ category, products, isLoading }: IProps) => {
-  console.log('products', products);
+export const ProductsSection = ({
+  category,
+  products,
+  isLoading,
+  withPriority = 4,
+  skeletons = 4,
+}: IProps) => {
   return (
-    <section className="flex flex-col gap-5">
+    <section className="flex flex-col gap-5 overflow-hidden">
       <h3 className="typo-title-24 capitalize">{category}</h3>
       <ul className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {isLoading
-          ? Array.from({ length: 9 }).map((_, index) => (
+          ? Array.from({ length: skeletons }).map((_, index) => (
               <ProductCardSkeleton key={index} />
             ))
           : products.map((product, i) => (
-              <li key={product.id}>
-                <ProductsCard
-                  variant={product}
-                  isPriority={i < 8}
-                />
-              </li>
+              <ProductsCard
+                key={product.id}
+                variant={product}
+                isPriority={i < withPriority}
+              />
             ))}
       </ul>
     </section>
