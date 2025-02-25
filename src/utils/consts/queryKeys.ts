aba264e661148335a9ffddf279fc6e7fc4dbe96c
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-02-25 14:34:31 +0200

    feat: added new queryKeys for tanstack query

--- src/utils/consts/queryKeys.ts
+++ src/utils/consts/queryKeys.ts
@@ -2,5 +2,10 @@ export const queryKeys = {
   session: 'session',
   filters: 'filters',
   products: 'products',
+  cart: 'cart',
+  favorite: 'favorite',
   addCartItem: 'addCartItem',
+  removeCartItem: 'removeCartItem',
+  updateCartItem: 'updateCartItem',
+  toggleFavoriteItem: 'toggleFavoriteItem',
 };
