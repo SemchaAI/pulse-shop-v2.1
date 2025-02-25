1f1e43630ae394b7cb134da101b357304c16a253
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-02-25 14:33:14 +0200

    chore: removed unused function export

--- src/utils/helpers/index.ts
+++ src/utils/helpers/index.ts
@@ -2,7 +2,6 @@ export { getServerSession } from './getServerSession';
 export { userSessionAdapter } from './userSessionAdapter';
 export { formUrlQuery } from './fromUrlQuery';
 // export { removeUrlQuery } from './removeUrlQuery';
-export { debounce } from './debounce';
 export { cartHelper } from './cartHelper';
 export { favoriteHelper } from './favoriteHelper';
 export { updateCartTotalAmount } from './updateCartTotal';
