b71e776055b1d2968fed1146c310b92e6a214d19
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-02-25 16:56:04 +0200

    feat: optimize carousel with memoization and responsive adjustments
    -fixed adaptive ui

--- src/components/entities/products/ProductsCarousel.tsx
+++ src/components/entities/products/ProductsCarousel.tsx
@@ -9,56 +9,66 @@ import {
 } from '@/components/features';
 
 import type { ISearchProductVariant } from '@/models/prisma';
+import { memo } from 'react';
 
 type PropType = {
   products: ISearchProductVariant[];
   options?: EmblaOptionsType;
 };
 
-export const ProductsCarousel: React.FC<PropType> = (props) => {
-  const { products, options } = props;
-  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+export const ProductsCarousel: React.FC<PropType> = memo(
+  ({
+    products,
+    options = { align: 'center', slidesToScroll: 'auto' } as EmblaOptionsType,
+  }) => {
+    const [emblaRef, emblaApi] = useEmblaCarousel(options);
 
-  const {
-    prevBtnDisabled,
-    nextBtnDisabled,
-    onPrevButtonClick,
-    onNextButtonClick,
-  } = usePrevNextButtons(emblaApi);
+    const {
+      prevBtnDisabled,
+      nextBtnDisabled,
+      onPrevButtonClick,
+      onNextButtonClick,
+    } = usePrevNextButtons(emblaApi);
 
-  return (
-    <div className="relative m-auto max-w-custom-xl w-full">
-      <div
-        className="overflow-hidden"
-        ref={emblaRef}
-      >
-        <div className="flex backface-hidden touch-pan-y gap-4 my-1">
-          {products.map((product, i) => {
-            return (
-              <div
-                className="h-auto transform translate-x-0 min-w-0 flex-shrink-0 flex-grow-0 basis-[80%] sm:basis-[calc(50%-0.5rem)] md:basis-[calc(33.33%-0.66rem)] lg:basis-[calc(25%-0.75rem)]"
-                key={product.id}
-              >
-                <ProductsCard
-                  isPriority={i < 8}
-                  variant={product}
-                />
-              </div>
-            );
-          })}
+    return (
+      <div className="m-auto w-full relative">
+        <div
+          className="overflow-hidden"
+          ref={emblaRef}
+        >
+          {/* -ml-4  pl-4 need to be as our main container px-[4px] sm:px-[8px] lg:px-[16px]*/}
+          <div className="flex touch-pan-y touch-pinch-zoom -ml-1 sm:-ml-2 lg:-ml-4">
+            {products.map((product, i) => {
+              return (
+                <ul
+                  className="transform-gpu min-w-0 pl-1 sm:pl-2 lg:pl-4
+                  flex-shrink-0 flex-grow-0 
+                  basis-full sm:basis-1/2 md:basis-1/2 lg:basis-1/3 xl:basis-1/4"
+                  key={product.id}
+                >
+                  <ProductsCard
+                    isPriority={i < 8}
+                    variant={product}
+                  />
+                </ul>
+              );
+            })}
+          </div>
+        </div>
+
+        <div className="absolute right-0 left-0 top-[calc(50%-17.5px)] flex justify-between items-center pointer-events-none">
+          <PrevButton
+            onClick={onPrevButtonClick}
+            disabled={prevBtnDisabled}
+          />
+          <NextButton
+            onClick={onNextButtonClick}
+            disabled={nextBtnDisabled}
+          />
         </div>
       </div>
+    );
+  }
+);
 
-      <div className="absolute right-0 left-0 top-[calc(50%-17.5px)] flex justify-between items-center pointer-events-none">
-        <PrevButton
-          onClick={onPrevButtonClick}
-          disabled={prevBtnDisabled}
-        />
-        <NextButton
-          onClick={onNextButtonClick}
-          disabled={nextBtnDisabled}
-        />
-      </div>
-    </div>
-  );
-};
+ProductsCarousel.displayName = 'ProductsCarousel';
