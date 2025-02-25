337f5a25fe0ac44953273772d6622a96c6a8aa63
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-02-25 16:27:20 +0200

    refactor: changed category and product handling

--- src/components/widgets/productsSection/ProductsCarouselSection.tsx
+++ src/components/widgets/productsSection/ProductsCarouselSection.tsx
@@ -1,41 +1,31 @@
-import { ProductsCarousel } from '@/components/entities';
-import { Container } from '@/components/shared';
-import type { ICategory } from '@/models/prisma';
 import Link from 'next/link';
 
+import { ProductsCarousel } from '@/components/entities';
+import { Container } from '@/components/shared';
+import type { ISearchProductVariant } from '@/models/prisma';
+
 interface IProps {
-  category: ICategory;
+  products: ISearchProductVariant[];
+  category: string;
 }
 
-export const ProductsCarouselSection = ({ category }: IProps) => {
-  if (!category.products || !category.products.length) {
+export const ProductsCarouselSection = ({ category, products }: IProps) => {
+  if (!category || !category.length) {
     return null;
   }
-  const products = category.products
-    .map((product) => {
-      const [firstVariant] = product.variants;
-      return firstVariant ? { ...firstVariant, images: product.images } : null;
-    })
-    .filter((product) => product !== null);
-
   return (
     <section className="my-3">
       <Container>
-        <div className="flex flex-col w-full">
-          <div className="flex justify-between items-center">
-            <h3 className="typo-title-30">{category.name}</h3>
-            <Link
-              className="text-primary"
-              href={`/categories/${category.name.toLowerCase()}`}
-            >
-              View All
-            </Link>
-          </div>
-          <ProductsCarousel
-            products={products}
-            options={{ align: 'start' }}
-          />
+        <div className="flex justify-between items-center">
+          <h3 className="typo-title-30">{category}</h3>
+          <Link
+            className="text-primary"
+            href={`/categories/${category.toLowerCase()}`}
+          >
+            View All
+          </Link>
         </div>
+        <ProductsCarousel products={products} />
       </Container>
     </section>
   );
