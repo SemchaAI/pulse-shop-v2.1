a20ff1f99c2284fc53a63c3d702bdea9dc441e0f
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-02-25 14:12:29 +0200

    feat: config for main navbar

---
+++ src/utils/consts/nav.ts
@@ -0,0 +1,24 @@
+import { ROUTES } from './routes';
+
+export const HeaderNav = [
+  {
+    name: 'Home',
+    href: ROUTES.HOME,
+  },
+  {
+    name: 'All Products',
+    href: ROUTES.ALL,
+  },
+  {
+    name: 'Tablets',
+    href: ROUTES.TABLETS,
+  },
+  {
+    name: 'Smartphones',
+    href: ROUTES.SMARTPHONES,
+  },
+  {
+    name: 'Accessories',
+    href: ROUTES.ACCESSORIES,
+  },
+];
