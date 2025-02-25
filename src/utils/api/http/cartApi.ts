6882e953be1eac25928dd05bc8c5fb56cf212858
Semciuc Nichita <nikitasemciuc@gmail.com>
2025-02-25 14:52:36 +0200

    refactor(cart): replace Redux async thunks with API functions
    - Removed `createAsyncThunk` for cart operations.
    - Introduced standalone functions for `getCart`, `addCartItem`, `removeCartItem`, and `updateItemQuantity`.

--- src/utils/api/http/cartApi.ts
+++ src/utils/api/http/cartApi.ts
@@ -1,54 +1,13 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-
+// import { createAsyncThunk } from '@reduxjs/toolkit';
 import { API_ROUTES } from '@/utils/consts';
 import { customFetch } from '@/utils/api';
-import { getServerSession } from '@/utils/helpers';
 import type { ICartItem, ICartSliceData } from '@/models/cart';
 
-// import type { CreateItem, ICart } from '@/models/cartFavor';
-// import { IUserSession } from '@/models/user';
-
-// export const fetchCart = createAsyncThunk<ICartResponse | null, void>(
-//   'cart/fetchCart', // action type string
-//   async (_, thunkAPI) => {
-//     try {
-//       const res = await customFetch(API_ROUTES.CART);
-//       if (!res.ok) throw new Error('Failed to fetch cart');
-//       const cart = await res.json();
-//       console.log('Fetched cart:', cart);
-
-//       return cart as ICartResponse;
-//     } catch (error) {
-//       console.error('Error fetching cart:', error);
-//       return thunkAPI.rejectWithValue('Failed to fetch cart');
-//     }
-//   }
-// );
-
-export const addCartItem = createAsyncThunk<
-  ICartSliceData,
-  ICartItem,
-  { rejectValue: string }
->('cart/addCartItem', async (item, thunkAPI) => {
-  try {
-    const { user } = await getServerSession();
-    if (!user) {
-      //await signIn('credentials', { redirect: false }, 'anon=true');
-    }
-    const res = await customFetch(API_ROUTES.CART, {
-      method: 'POST',
-      body: JSON.stringify(item),
-    });
-    if (!res.ok) throw new Error('Failed to add item');
-    const cart = await res.json();
-    return cart as ICartSliceData;
-  } catch (error) {
-    console.error('Error adding item:', error);
-    return thunkAPI.rejectWithValue('Failed to add item');
-  }
-});
-
-// export async function addCartItem(item: ICartItem): Promise<ICart> {
+// export const addCartItem = createAsyncThunk<
+//   ICartSliceData,
+//   ICartItem,
+//   { rejectValue: string }
+// >('cart/addCartItem', async (item, thunkAPI) => {
 //   try {
 //     const { user } = await getServerSession();
 //     if (!user) {
@@ -60,9 +19,69 @@ export const addCartItem = createAsyncThunk<
 //     });
 //     if (!res.ok) throw new Error('Failed to add item');
 //     const cart = await res.json();
-//     return cart as ICart;
+//     return cart as ICartSliceData;
 //   } catch (error) {
-//     console.error('Error adding item:', error);
-//     throw error;
+//     console.log('Error adding item:', error);
+//     return thunkAPI.rejectWithValue('Failed to add item');
 //   }
-// }
+// });
+export const addCartItem = async (item: ICartItem) => {
+  try {
+    const res = await customFetch(API_ROUTES.CART, {
+      method: 'POST',
+      body: JSON.stringify(item),
+    });
+    if (!res.ok) throw new Error('Failed to add item');
+    const cart = await res.json();
+    return cart as ICartSliceData;
+  } catch (error) {
+    console.log('[ADD TO CART]:', error);
+    throw new Error('[ADD TO CART]: Something went wrong');
+  }
+};
+
+export const getCart = async (): Promise<ICartSliceData> => {
+  try {
+    const res = await customFetch(API_ROUTES.CART);
+    if (!res.ok) throw new Error('Failed to get cart');
+    return await res.json();
+  } catch (error) {
+    console.log('Failed to get cart:', error);
+    throw new Error('Failed to get cart');
+  }
+};
+
+export const removeCartItem = async (id: number): Promise<ICartSliceData> => {
+  try {
+    const res = await customFetch(`${API_ROUTES.CART}/${id}`, {
+      method: 'DELETE',
+      body: JSON.stringify({ id }),
+    });
+    if (!res.ok) throw new Error('Failed to remove item');
+
+    return await res.json();
+  } catch (error) {
+    console.log('Error removing cart item:', error);
+    throw new Error('Failed to remove item');
+  }
+};
+
+export const updateItemQuantity = async (
+  id: number,
+  quantity: number
+): Promise<ICartSliceData> => {
+  try {
+    const res = await customFetch(`${API_ROUTES.CART}/${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify({ id, quantity }),
+    });
+    if (!res.ok) {
+      throw new Error('Failed to update item quantity');
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.error('Error updating item quantity:', error);
+    throw new Error('Failed to update item quantity');
+  }
+};
